38043c30cc25db70da1daece37304aa89edb6866
bths-ax <ardenx@nycstudents.net>
2022-09-28 08:41:23 -0400

    oops forgot to check all the changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Scanning for values
+        System.out.print("Enter the bill: ");
+        double bill = scanner.nextDouble();
+
+        System.out.print("Enter the tip percentage: ");
+        double tipPercentage = scanner.nextDouble();
+
+        System.out.print("Enter the number of people: ");
+        int numPeople = scanner.nextInt();
+
+        // Calculating values
+        double tipAmount = bill * (tipPercentage / 100.0);
+        double totalBill = bill + tipAmount;
+
+        double tipAmountPerPerson = tipAmount / numPeople; // java implicit conversion from int -> double
+        double totalBillPerPerson = totalBill / numPeople; // makes this division fine
+
+        // Printing results
+        System.out.println();
+        System.out.println("Total tip amount: $" + String.format("%.2f", tipAmount + 0.005)); // add 0.005 to force formatter to always round *up*
+        System.out.println("Total bill: $" + String.format("%.2f", totalBill + 0.005));       // this is because we are dealing with money
+
+        System.out.println();
+        System.out.println("Tip per person: $" + String.format("%.2f", tipAmountPerPerson + 0.005));
+        System.out.println("Total per person: $" + String.format("%.2f", totalBillPerPerson + 0.005));
     }
 }
